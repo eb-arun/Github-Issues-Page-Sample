80b51fc519b7549a8a56c3959a0c607f055ecef8
Arun EB <eb.arun@gmail.com>
2018-07-31 19:06:48 +0530

    Angualr Integration + Components Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { IssuesComponent } from './issues/issues.component';
+import { CodeComponent } from './code/code.component';
+import { WikiComponent } from './wiki/wiki.component';
+import { PullRequestComponent } from './pull-request/pull-request.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { InsightsComponent } from './insights/insights.component';
+import { TimeAgoPipe } from 'time-ago-pipe';
+
+export const rootRouterConfig: Routes = [
+    { path: '', redirectTo: 'Issues' , pathMatch: 'full'},
+    { path: 'Issues', component:IssuesComponent},
+    { path: 'Wiki', component:WikiComponent},
+    { path: 'Projects', component:ProjectsComponent},
+    { path: 'PullRequests', component:PullRequestComponent},
+    { path: 'Code', component:CodeComponent},
+    { path: 'Insights', component:InsightsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssuesComponent,
+    CodeComponent,
+    WikiComponent,
+    PullRequestComponent,
+    ProjectsComponent,
+    InsightsComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    HttpClientModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(rootRouterConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
